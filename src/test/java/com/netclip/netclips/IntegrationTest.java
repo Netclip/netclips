542e239ac4bcdb42da089d34387b431310fcc66a
Jiayong Lin <96598259+linjiayo@users.noreply.github.com>
2022-08-22 14:34:40 -0400

    scaffold jhipster (#1)

---
+++ src/test/java/com/netclip/netclips/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.netclip.netclips;
+
+import com.netclip.netclips.NetclipsApp;
+import com.netclip.netclips.config.AsyncSyncConfiguration;
+import com.netclip.netclips.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { NetclipsApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
