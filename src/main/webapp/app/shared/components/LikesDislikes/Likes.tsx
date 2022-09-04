95389e34006b7a8fae08e64b1b96d10e3b6fe0a9
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-09-04 15:17:39 -0400

    finally getting likes/dislikes data, needs initial state value on page load functionality & some single responsibility refactoring. work in progress

--- src/main/webapp/app/shared/components/LikesDislikes/Likes.tsx
+++ src/main/webapp/app/shared/components/LikesDislikes/Likes.tsx
@@ -3,62 +3,16 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './Likes.scss';
 
+// const Count = ({ count }) => {
+//   const [num, setNum] = useState(count);
+
+//   useEffect(() => {
+//     setNum(count);
+//   }, [count]);
+//   return <p>{num}</p>
+// }
+
 export const Like = props => {
-  var [likeCount, setLikeCount] = useState([]);
-  var [dislikeCount, setDislikeCount] = useState([]);
-  var [vidId, setVidId] = useState(window.location.href.slice(-1));
-  var [likeStatus, setLikeStatus] = useState(false);
-
-  const getBearerToken = () => {
-    var authToken = localStorage.getItem('jhi-authenticationToken') || sessionStorage.getItem('jhi-authenticationToken');
-    if (authToken) {
-      authToken = JSON.parse(authToken);
-      return `Bearer ${authToken}`;
-    }
-    return null;
-  };
-
-  const axiosConfig = {
-    timeout: 5000,
-    headers: {
-      Authorization: getBearerToken(),
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    },
-  };
-  const fetchData = async () => {
-    try {
-      await axios
-        .get('api/videos', axiosConfig)
-        // .then(response => setLikeCount(response.data[0]))
-        // console.log(likeCount)
-        .then(response => {
-          // setLikeCount(response.data)
-          console.log(likeCount);
-          setVidId(vidId);
-          // console.log("-> "+window.location.href.slice(-1))
-          console.log(response.data[Number.parseInt(vidId) - 1].likes);
-          setLikeCount(response.data[Number.parseInt(vidId) - 1].likes);
-          setDislikeCount(response.data[Number.parseInt(vidId) - 1].dislikes);
-          // setLikeCount(response)
-          // setLikeCount(++likeCount);
-          setLikeStatus(true);
-        });
-
-      if (likeStatus == true) {
-        // setLikeCount(--likeCount);
-        setLikeStatus(false);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-
-    useEffect(() => {
-      setLikeCount(likeCount);
-      setDislikeCount(dislikeCount);
-    }, [props]);
-  };
-
   // function incrementLikes() {
   //   likeCount = props.likesCount;
   //     setLikeCount(++likeCount);
@@ -73,28 +27,17 @@ export const Like = props => {
   //       setLikeCount(likeCount);
   //     });
   //   }
-
   return (
     <div
       className="button"
-      onClick={fetchData}
+      onClick={props.function}
       style={{
-        backgroundColor: likeStatus ? 'red' : '',
-        color: likeStatus ? 'black' : '',
+        backgroundColor: props.conditional ? 'red' : '',
+        color: props.conditional ? 'black' : '',
       }}
     >
       {props.text}: <FontAwesomeIcon icon={props.icon} />
-      {/* <div>{likeCount['likes']}</div> */}
-      <div>
-        {/* {likeCount.map((el, i) => {
-        return (
-          <p>
-            {el['likes']}
-          </p>
-        )
-      })} */}
-        {props.likesCount}
-      </div>
+      <div>{props.count}</div>
     </div>
   );
 };
