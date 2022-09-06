e4a7f8d2425ddf93aaf128d2207b18ef2791cf80
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-09-05 21:30:59 -0400

    working comments

--- src/main/webapp/app/shared/components/VideoComments/video-comments.tsx
+++ src/main/webapp/app/shared/components/VideoComments/video-comments.tsx
@@ -2,46 +2,33 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Like from '../LikesDislikes/Likes';
 import './video-comments.scss';
+import Moment from 'react-moment';
 
 const API_URL = '/api/comments';
 
-function VideoComments() {
+function VideoComments(props) {
+  const id = props.id;
+
   const [comment, setComments] = useState([]);
   const [content, setContent] = useState('');
-  const [video_id, setVideo_id] = useState<Number>();
   const axios = require('axios');
 
   const getComments = async () => {
     try {
-      const response = await axios.get(`${API_URL}`);
-      console.log(response.data);
-      setComments(response.data);
+      const response = await axios.get(`${API_URL}/video/?video_id=${id}`);
+      console.log(response.data.content);
+      setComments(response.data.content);
     } catch (err) {
       console.log(err);
     }
   };
-
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-  //   const url = `${API_URL}/post?video_id=1`;
-  //   const formData = new FormData();
-  //   formData.append('content', content);
-  //   const config = {
-  //     headers: {
-  //       'content-type': 'multipart/form-data',
-  //     },
-  //   };
-  //   axios.post(url, formData, config).then(response => {
-  //     console.log(response.data), alert('Posted Comment');
-  //   });
-  // }
 
   function handleSubmit(event) {
     event.preventDefault();
     axios
       .post(API_URL + '/post', JSON.stringify(content), {
         headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-        params: { video_id: 4, content: content }, //Add userID as a param
+        params: { video_id: props.id, content: content }, //Add userID as a param
       })
       .then(response => console.log('repsonse', response.status));
   }
@@ -61,7 +48,7 @@ function VideoComments() {
         </form>
         {/* </textarea> */}
 
-        <div className="buttons-container">
+        {/* <div className="buttons-container">
           <div className="comment-button-container">
             <div className="submit-comments" onClick={() => {}}>
               submit
@@ -71,7 +58,7 @@ function VideoComments() {
             <Like text={'Likes'} icon={'heart'} />
             <Like text={'Dislikes'} icon={'trash'} />
           </div>
-        </div>
+        </div> */}
       </div>
       <div>
         {comment.map((comment, index) => {
@@ -79,20 +66,20 @@ function VideoComments() {
           return (
             <div key={index}>
               <div className="recommendedVideos__videos">
-                <div className="post-card" key={comment.id}>
-                  <h2 className="post-title">{comment.timeStamp}</h2>
-                  <p className="post-body">{comment.content}</p>
+                <div key={comment.id}>
+                  <h4>{comment.content}</h4>
+                  <div>
+                    {comment.uploader} --
+                    <Moment format="YYYY/MM/DD HH:mm">{comment.timeStamp}</Moment>
+                  </div>
+                  <p>
+                    {comment.likes}
+                    {comment.dislikes}
+                  </p>
                   {/* <div className="button">
                     <div className="delete-btn">Delete</div>
                   </div> */}
                 </div>
-                {/* <p>
-                  {comment.id}
-                  {comment.content}
-                  {comment.timeStamp}
-                  {comment.likes}
-                  {comment.dislikes}
-                </p> */}
               </div>
             </div>
           );
